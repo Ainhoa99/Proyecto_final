f144b860bcef4ee9d798f0e0c5e96f9d3231c604
Xabier <xabier.trabajo.69@gmail.com>
2023-02-24 10:31:34 +0100

    links vistas

--- ahorcado/src/components/juegoAhorcado.vue
+++ ahorcado/src/components/juegoAhorcado.vue
@@ -1,9 +1,13 @@
 <template>
     <p>
+        - Este componente tiene que ir en una nueva vista que debe sustituir a la del menu principal <br> <br>
         links: <br>
         - juego: <a href="https://parzibyte.me/blog/2020/09/24/ahorcado-juego-javascript/"> el ahorcado</a> <br>
-        - cambiar vistas: <a href="https://es.stackoverflow.com/questions/504696/cambiar-de-vista-vue-a-través-de-botón"> cambiar vistas</a>  <br>
-        - Este componente tiene que ir en una nueva vista que debe sustituir a la del menu principal
+        - cambiar vistas: <br>
+        https://router.vuejs.org/guide/ <br>
+        https://www.vuemastery.com/blog/vue-router-a-tutorial-for-vue-3/ <br>
+
+       
     </p>
 </template>
 
